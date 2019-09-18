e4ff647affecd235c3f2b8890ac02856110161d8
eiphi <jazdzewski.maciej@gmail.com>
2019-09-19 00:08:36 +0200

    initial setup, dependecies, semantic ui etc.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        dsadasdas
+        <Button>Hey there</Button>
+      </header>
+    </div>
+  );
+};
+
+export default App;
